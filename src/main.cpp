c1fbc29e044e81444a5f92b529bc841084d9d313
Netiozo7 <therealnetiozo4@gmail.com>
2026-05-19 19:11:23 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,12 @@ class $modify(MyMenuLayer, MenuLayer) {
             return false;
         }
 
-        // 128 partout = luminosité baissée de moitié (mode sombre)
         ccColor3B darkColor = {128, 128, 128};
 
-        // On applique la couleur au fond si possible
         if (auto rgbaNode = dynamic_cast<CCRGBAProtocol*>(this)) {
             rgbaNode->setColor(darkColor);
         }
 
-        // On parcourt tous les éléments de l'écran en toute sécurité
         for (auto child : CCArrayExt<CCNode*>(this->getChildren())) {
             if (auto rgbaChild = dynamic_cast<CCRGBAProtocol*>(child)) {
                 rgbaChild->setColor(darkColor);
